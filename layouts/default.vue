8e954ad5f51469f2768efbebf39f39d03c1559e5
tcyeee <tcyeee@outlook.com>
2025-01-08 15:59:41 +0800

    Update base style

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>title</div>
+  <NuxtPage class="top-[4rem]" />
+</template>
+<style>
+:root {
+  overflow-x: hidden;
+  user-select: none;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Index
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+</style>
