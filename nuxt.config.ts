220a7514723b2eaffb6dc0e8794ad1575104dd7e
tcyeee <tcyeee@outlook.com>
2025-02-10 17:54:50 +0800

    添加谷歌登陆

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,20 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
     'pinia-plugin-persistedstate/nuxt',
-    '@element-plus/nuxt'
+    '@element-plus/nuxt',
+    'nuxt-vue3-google-signin'
   ],
   elementPlus: { /** Options */ },
   css: ['~/assets/css/styles.css'],
-  plugins: ['~/plugins/contextMenu.js'],
+  plugins: [
+    '~/plugins/fingerprint.js',
+    '~/plugins/keyListener.js',
+    '~/plugins/contextMenu.js'
+  ],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  tailwindcss: {}
+  tailwindcss: {},
+  googleSignIn: {
+    clientId: process.env.BOOKMARKIFY_GOOGLE_LOGIN_ID
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,26 @@
   <div class="i-t-10">
     index test 11
   </div>
+
+  <GoogleSignInButton @success="handleLoginSuccess" @error="handleLoginError"></GoogleSignInButton>
 </template>
 
 <script lang="ts" setup>
+import {
+  GoogleSignInButton,
+  type CredentialResponse,
+} from "vue3-google-signin";
+
+// handle success event
+const handleLoginSuccess = (response: CredentialResponse) => {
+  const { credential } = response;
+  console.log("Access Token", credential);
+};
+
+// handle an error event
+const handleLoginError = () => {
+  console.error("Login failed");
+};
 </script>
 
 <style>
