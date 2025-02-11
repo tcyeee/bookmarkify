140077904a7dffe6e6a021209f02e2fbcfa33f9c
tcyeee <tcyeee@outlook.com>
2025-02-11 13:27:42 +0800

    temporary

--- pages/index.vue
+++ pages/index.vue
@@ -7,10 +7,12 @@ import { useOneTap, type CredentialResponse } from "vue3-google-signin";
 
 useOneTap({
   onSuccess: (response: CredentialResponse) => {
-    console.log("Success:", response);
+    console.log("Success:", response.credential);
   },
-  onError: () => console.error("=====Error with One Tap Login"),
+  onError: () => console.error("Error with One Tap Login"),
 });
+
+console.log("================================" + process.env.NODE_ENV);
 </script>
 
 <style>
