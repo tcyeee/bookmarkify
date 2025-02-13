c7ef8b002ca3bf598f2b480f0affe2737dc5c78f
tcyeee <tcyeee@outlook.com>
2025-02-13 15:59:29 +0800

    Update Google Login Trigger Location

--- app.vue
+++ app.vue
@@ -4,30 +4,3 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-<script  lang="ts" setup>
-// @ts-ignore
-import { useOneTap, type CredentialResponse } from "vue3-google-signin";
-import { Environments } from "~/types/environments";
-import { StoreUser } from "@/stores/user.store";
-const storeUser = StoreUser();
-
-onMounted(() => {
-  googleLogin();
-});
-
-function googleLogin() {
-  if (process.env.NODE_ENV != Environments.PRO) return;
-  if (storeUser.auth.googleId) return;
-  console.log("[google]======start");
-
-  useOneTap({
-    onSuccess: (response: CredentialResponse) => {
-      console.log("======================[google]==========");
-      console.log(response);
-      console.log(response.credential);
-      console.log("======================[google]==========");
-    },
-    onError: () => console.error("Error with One Tap Login"),
-  });
-}
-</script>
