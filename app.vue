eb44a925fd3ef7c4b198f17ce75811bfceb68549
tcyeee <tcyeee@outlook.com>
2025-02-13 15:52:19 +0800

    测试谷歌校验

--- app.vue
+++ app.vue
@@ -16,18 +16,14 @@ onMounted(() => {
 });
 
 function googleLogin() {
-  console.log("== 01" + process.env.NODE_ENV);
   if (process.env.NODE_ENV != Environments.PRO) return;
-  console.log("== 02" + storeUser.auth.googleId);
   if (storeUser.auth.googleId) return;
-  console.log("== 03");
   useOneTap({
     onSuccess: (response: CredentialResponse) => {
-      const jwt = response.credential; // 获取 JWT
-      const base64Url = jwt.split(".")[1]; // 获取负载部分
-      const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-      const decodedData = JSON.parse(window.atob(base64)); // 解码负载
-      console.log(decodedData); // 打印解码后的数据
+      console.log("======================[google]==========");
+      console.log(response);
+      console.log(response.credential);
+      console.log("======================[google]==========");
     },
     onError: () => ElNotification.error("谷歌API调用失败!"),
   });
