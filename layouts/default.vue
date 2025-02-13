a9d6d82c869cd68ea6a07059e7476cc6ce152791
tcyeee <tcyeee@outlook.com>
2025-02-13 15:59:29 +0800

    更新谷歌登陆触发位置

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,32 @@
 <template>
   <NuxtPage />
 </template>
+<script  lang="ts" setup>
+// @ts-ignore
+import { useOneTap, type CredentialResponse } from "vue3-google-signin";
+import { Environments } from "~/types/environments";
+import { StoreUser } from "@/stores/user.store";
+const storeUser = StoreUser();
+
+onMounted(() => {
+  googleLogin();
+});
+
+function googleLogin() {
+  if (process.env.NODE_ENV != Environments.PRO) return;
+  if (storeUser.auth.googleId) return;
+  console.log("[google]======start");
+
+  useOneTap({
+    onSuccess: (response: CredentialResponse) => {
+      console.log("======================[google]==========");
+      console.log(response);
+      console.log(response.credential);
+      console.log("======================[google]==========");
+    },
+    onError: () => console.error("Error with One Tap Login"),
+  });
+}
+</script>
 <style>
 </style>
