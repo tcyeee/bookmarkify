4f65e9e6ce535b23d1844c402dbd4c097e554659
tcyeee <tcyeee@outlook.com>
2025-02-13 15:42:41 +0800

    更新谷歌登陆方法

--- pages/index.vue
+++ pages/index.vue
@@ -3,19 +3,6 @@
 </template>
 
 <script lang="ts" setup>
-// @ts-ignore
-import { useOneTap, type CredentialResponse } from "vue3-google-signin";
-import { Environments } from "~/types/environments";
-
-// 仅在生产环境进行谷歌登陆
-if (process.env.NODE_ENV == Environments.PRO) {
-  useOneTap({
-    onSuccess: (response: CredentialResponse) => {
-      console.log("Success:", response.credential);
-    },
-    onError: () => console.error("Error with One Tap Login"),
-  });
-}
 </script>
 
 <style>
