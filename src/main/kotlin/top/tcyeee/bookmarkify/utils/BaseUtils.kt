263cab485f5e01dd90d5f860045b665f96f4e49b
tcyeee <tcyeee@outlook.com>
2025-03-16 19:46:06 +0800

    手动进行JSON格式化

--- src/main/kotlin/top/tcyeee/bookmarkify/utils/BaseUtils.kt
+++ src/main/kotlin/top/tcyeee/bookmarkify/utils/BaseUtils.kt
@@ -3,7 +3,6 @@ package top.tcyeee.bookmarkify.utils
 import cn.dev33.satoken.session.SaSession
 import cn.dev33.satoken.stp.StpUtil
 import cn.hutool.core.date.LocalDateTimeUtil
-import cn.hutool.json.JSONUtil
 import top.tcyeee.bookmarkify.entity.dto.UserInfo
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
@@ -28,7 +27,6 @@ object BaseUtils {
         val session = StpUtil.getSessionByLoginId(loginId) ?: return null
         return user(session).uid
     }
-
 
     /* 工具类方法 */
     fun yesterday(): LocalDateTime = LocalDateTimeUtil.offset(LocalDateTime.now(), -1, ChronoUnit.DAYS)
