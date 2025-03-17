2be1597a07f4b5b22d282534eafdc2af07c7b874
tcyeee <tcyeee@outlook.com>
2025-03-17 23:51:30 +0800

    修复图片添加BUG

--- src/main/kotlin/top/tcyeee/bookmarkify/mapper/BookmarkUserLinkMapper.kt
+++ src/main/kotlin/top/tcyeee/bookmarkify/mapper/BookmarkUserLinkMapper.kt
@@ -48,7 +48,8 @@ interface BookmarkUserLinkMapper : BaseMapper<BookmarkUserLink>{
                    b.is_activity   as isActivity,
                    b.icon_activity as iconActivity,
                    b.icon_hd       as iconHd,
-                   a.url_full      as urlFull
+                   a.url_full      as urlFull,
+                   b.icon_url      as iconUrl
             from bookmarkify.bookmark_user_link a
                      left join bookmarkify.bookmark b on a.bookmark_id = b.id
             where a.id = #{id}
