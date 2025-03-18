4aeaa312333153aa08c4de12a13b57ab92818606
tcyeee <tcyeee@outlook.com>
2025-03-18 17:50:25 +0800

    Directory Structure Modification

--- components/bookmark/cell/Loading.vue
+++ components/bookmark/cell/Loading.vue
@@ -1,14 +1,11 @@
 <template>
-  <!-- 等待回执样式 -->
-  <div class="w-[5rem] flex flex-col items-center">
-    <div class="w-app h-app rounded-xl bg-gray-100 center shadow overflow-hidden">
-      <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24">
-        <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-          <rect width="20" height="8" x="2" y="6" rx="1" />
-          <path d="M17 14v7M7 14v7M17 3v3M7 3v3m3 8L2.3 6.3M14 6l7.7 7.7M8 6l8 8" />
-        </g>
-      </svg>
-    </div>
-    <div class="w-[4.5rem] text-sm mt-[0.3rem] text-white opacity-90  truncate text-center">loading..</div>
+  <div class="bookmark-icon">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24">
+      <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+        <rect width="20" height="8" x="2" y="6" rx="1" />
+        <path d="M17 14v7M7 14v7M17 3v3M7 3v3m3 8L2.3 6.3M14 6l7.7 7.7M8 6l8 8" />
+      </g>
+    </svg>
   </div>
+  <div class="w-[4.5rem] text-sm mt-[0.3rem] text-white opacity-90  truncate text-center">loading..</div>
 </template>
