941960741fa202e09277233f1960c0f75e1defc3
tcyeee <tcyeee@outlook.com>
2025-03-18 20:05:54 +0800

    Detail optimization

--- components/bookmark/cell/Loading.vue
+++ components/bookmark/cell/Loading.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="bookmark-icon">
-    <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24">
-      <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-        <rect width="20" height="8" x="2" y="6" rx="1" />
-        <path d="M17 14v7M7 14v7M17 3v3M7 3v3m3 8L2.3 6.3M14 6l7.7 7.7M8 6l8 8" />
-      </g>
-    </svg>
+    <span class="icon--loading" />
   </div>
-  <div class="w-[4.5rem] text-sm mt-[0.3rem] text-white opacity-90  truncate text-center">loading..</div>
+  <div class="text-gray-300 w-[4.5rem] text-sm mt-[0.3rem]   truncate text-center">loading...</div>
 </template>
