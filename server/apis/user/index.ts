c9f172fe07781b8d36af7f42ef4507feb893283f
tcyeee <tcyeee@outlook.com>
2025-03-19 15:59:16 +0800

    Add account deactivation

--- server/apis/user/index.ts
+++ server/apis/user/index.ts
@@ -3,3 +3,4 @@ import type * as types from './typing';
 
 export const queryUserInfo = () => http.get("/user/info") as Promise<types.UserInfoShow>;
 export const updateUserInfo = (param: types.UserInfoUpdate) => http.post("/user/updateInfo", param) as Promise<boolean>;
+export const accountDelete = (pwd: string) => http.post("/user/del", { password: btoa(pwd) }) as Promise<boolean>;
