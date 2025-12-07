2197a01a34c96823a7892e854b01eb82caa58e93
tcyeee <tcyeee@outlook.com>
2025-12-07 17:11:58 +0800

    Refactor UserFile and UserInfoShow to include avatar file management; update UserServiceImpl to fetch avatar details from FileMapper

--- bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/entity/response/UserInfoShow.kt
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/entity/response/UserInfoShow.kt
@@ -2,6 +2,7 @@ package top.tcyeee.bookmarkify.entity.response
 
 import cn.hutool.core.bean.BeanUtil
 import top.tcyeee.bookmarkify.entity.entity.UserEntity
+import top.tcyeee.bookmarkify.entity.entity.UserFile
 
 data class UserInfoShow(
     var uid: String,
@@ -11,6 +12,7 @@ data class UserInfoShow(
     var email: String? = null,
     var avatarFileId: String? = null,
     var verified: Boolean? = null,       // 是否为验证过的账户(例如绑定手机号,绑定邮箱等)
+    var avatar: UserFile? = null,
 ) {
     constructor(user: UserEntity, token: String) : this(
         uid = user.id,
