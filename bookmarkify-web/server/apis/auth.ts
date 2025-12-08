0310e85e4c74ae87c7f59dae5459d14e0db0a21b
tcyeee <tcyeee@outlook.com>
2025-12-08 12:24:28 +0800

    Remove unused authentication and HTTP API files

--- bookmarkify-web/server/apis/auth.ts
+++ bookmarkify-web/server/apis/auth.ts
@@ -1,5 +1,5 @@
 import type { UserInfoEntity } from '@typing'
-import http from '../http'
+import http from './http'
 
 export const authByDeviceInfo = (deviceId: string) =>
   http.get('/auth/loginByDeviceId', { deviceId: deviceId }) as Promise<UserInfoEntity>
