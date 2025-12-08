1637d3999b4f986053f9b1762e9517e587d52d2d
tcyeee <tcyeee@outlook.com>
2025-12-08 12:30:56 +0800

    Refactor authentication API by consolidating authByDeviceInfo and updating type naming conventions

--- bookmarkify-web/server/apis/index.ts
+++ bookmarkify-web/server/apis/index.ts
@@ -1,10 +1,13 @@
 import http from './http'
 import type * as t from '@typing'
-export * from './auth'
+
+/* =========[ /auth ]========= */
+export const authByDeviceInfo = (deviceId: string) =>
+  http.get('/auth/loginByDeviceId', { deviceId: deviceId }) as Promise<t.UserInfoEntity>
 
 /* =========[ /bookmark ]========= */
 export const bookmarksShowAll = () => http.post('/bookmark/query') as Promise<Array<t.HomeItem>>
-export const bookmarksAddOne = (params: t.bookmarksAddOneParams) => http.post('/bookmark/addOne', params) as Promise<t.HomeItem>
+export const bookmarksAddOne = (params: t.BookmarksAddOneParams) => http.post('/bookmark/addOne', params) as Promise<t.HomeItem>
 export const bookmarksSort = (params: Array<t.BookmarkSortParams>) => http.post('/bookmark/sort', params) as Promise<boolean>
 export const bookmarksDel = (params: Array<string>) => http.post('/bookmark/delete', params) as Promise<boolean>
 export const bookmarksUpdate = (params: t.BookmarkUpdatePrams) => http.post('/bookmark/update', params) as Promise<t.Bookmark>
