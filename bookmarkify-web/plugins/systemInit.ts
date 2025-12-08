b65a6810f4bc3d09cca65c6943f745a8ea8035b7
tcyeee <tcyeee@outlook.com>
2025-12-08 15:12:15 +0800

    Add system configuration management by implementing refreshSystemConfig in sys store and updating system initialization to fetch default settings

--- bookmarkify-web/plugins/systemInit.ts
+++ bookmarkify-web/plugins/systemInit.ts
@@ -3,9 +3,12 @@ export default defineNuxtPlugin(() => {
 
   const userStore = useUserStore()
   const bookmarkStore = useBookmarkStore()
+  const sysStore = useSysStore()
 
   // 获取用户信息
   void userStore.refreshUserInfo()
   // 获取书签
   void bookmarkStore.update()
+  // 获取系统默认配置信息
+  void sysStore.refreshSystemConfig()
 })
