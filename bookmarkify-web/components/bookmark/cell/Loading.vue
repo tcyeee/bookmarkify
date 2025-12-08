93fc7146093e2f76b4ef07a992b96003becca6f6
tcyeee <tcyeee@outlook.com>
2025-12-08 19:26:46 +0800

    Update icon styles

--- bookmarkify-web/components/bookmark/cell/Loading.vue
+++ bookmarkify-web/components/bookmark/cell/Loading.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bookmark-icon">
-    <span class="icon--loading" />
+    <span class="icon--loading icon-size-35 text-gray-400" />
   </div>
-  <div class="text-gray-300 w-[4.5rem] text-sm mt-[0.3rem]   truncate text-center">loading...</div>
+  <div class="text-gray-300 w-18 text-sm mt-[0.3rem] truncate text-center">loading...</div>
 </template>
--- bookmarkify-web/components/setting/account/cancel.vue
+++ bookmarkify-web/components/setting/account/cancel.vue
@@ -2,39 +2,22 @@
   <div>
     <!-- 注销账户 -->
     <div class="setting-title mt-25">注销账户</div>
-    <div class="setting-subtitle">
-      注销后账号所有数据将被销毁并不可找回，请谨慎操作。
-    </div>
-    <button class="cy-btn cy-btn-error rounded-2xl mt-2" @click="openDelDialog">
-      注销账户
-    </button>
+    <div class="setting-subtitle">注销后账号所有数据将被销毁并不可找回，请谨慎操作。</div>
+    <button class="cy-btn cy-btn-error rounded-2xl mt-2" @click="openDelDialog">注销账户</button>
 
     <!-- 注销账户对话框 -->
     <dialog id="userDelDialog" class="cy-modal">
       <div class="cy-modal-box">
         <div class="flex items-end gap-2">
-          <span class="icon--warning"></span>
-          <h3 class="text-lg font-bold text-red-500">
-            警告: 账户注销后将无法找回!
-          </h3>
+          <span class="icon--warning icon-size-22 text-red-500"></span>
+          <h3 class="text-lg font-bold text-red-500">警告: 账户注销后将无法找回!</h3>
         </div>
         <p class="mt-5 setting-subtitle">注销前需要验证密码:</p>
-        <input
-          v-model="data.password"
-          type="password"
-          placeholder="请输入密码"
-          class="cy-input"
-        />
+        <input v-model="data.password" type="password" placeholder="请输入密码" class="cy-input" />
 
         <div class="cy-modal-action">
           <button @click="closeDelDialog" class="cy-btn">取消</button>
-          <button
-            :disabled="data.password.length == 0"
-            class="cy-btn cy-btn-error"
-            @click="accountDel()"
-          >
-            确认注销!
-          </button>
+          <button :disabled="data.password.length == 0" class="cy-btn cy-btn-error" @click="accountDel()">确认注销!</button>
         </div>
       </div>
       <form method="dialog" class="cy-modal-backdrop">
@@ -45,32 +28,32 @@
 </template>
 
 <script lang="ts" setup>
-import { accountDelete } from "@api";
-const sysStore = useSysStore();
+import { accountDelete } from '@api'
+const sysStore = useSysStore()
 
 var data = reactive({
-  password: "",
-});
+  password: '',
+})
 
 function accountDel() {
-  accountDelete(data.password);
+  accountDelete(data.password)
 }
 
 function openDelDialog() {
-  if (!import.meta.client) return;
-  const dialog = document.getElementById("userDelDialog") as HTMLDialogElement;
-  if (!dialog) return;
-  dialog.showModal();
-  sysStore.preventKeyEventsFlag = true;
+  if (!import.meta.client) return
+  const dialog = document.getElementById('userDelDialog') as HTMLDialogElement
+  if (!dialog) return
+  dialog.showModal()
+  sysStore.preventKeyEventsFlag = true
 }
 
 function closeDelDialog() {
-  if (!import.meta.client) return;
-  const dialog = document.getElementById("userDelDialog") as HTMLDialogElement;
-  if (!dialog) return;
-  dialog.close();
-  sysStore.preventKeyEventsFlag = false;
-  data.password = "";
+  if (!import.meta.client) return
+  const dialog = document.getElementById('userDelDialog') as HTMLDialogElement
+  if (!dialog) return
+  dialog.close()
+  sysStore.preventKeyEventsFlag = false
+  data.password = ''
 }
 </script>
 
