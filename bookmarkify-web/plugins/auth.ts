4f1872842ebfa1b2a6b577f7a1869ed7683a1450
tcyeee <tcyeee@outlook.com>
2025-12-09 22:49:13 +0800

    refactor(auth): remove unnecessary store usage and cleanup plugin

--- bookmarkify-web/plugins/auth.ts
+++ bookmarkify-web/plugins/auth.ts
@@ -1,6 +1,5 @@
 export default defineNuxtPlugin(async () => {
   const userStore = useUserStore()
-  const bookmarkStore = useBookmarkStore()
   const webSocketStore = useWebSocketStore()
 
   // 先登录
@@ -8,7 +7,4 @@ export default defineNuxtPlugin(async () => {
   // 然后注册WebSocket
   if (!userStore.account?.token) throw new Error('[ERROR] 登录状态异常')
   webSocketStore.connect(userStore.account.token)
-  // 然后更新书签
-  await bookmarkStore.update()
-  // 然后更新用户基础信息
 })
