1a3b584ee4d9834b9fcb9a27f7163229aadf4f4f
tcyeee <tcyeee@outlook.com>
2025-12-09 16:15:42 +0800

    Refactor limitAction function to streamline timeLock management by removing client checks and simplifying timeLock handling for improved consistency across environments.

--- bookmarkify-web/server/utils/index.ts
+++ bookmarkify-web/server/utils/index.ts
@@ -1,7 +1,6 @@
 import { CurrentEnvironment } from '@typing'
 
-const isClient = import.meta.client
-let timeLockMemory = 0
+let timeLock: number = Number.MIN_SAFE_INTEGER
 
 /**
  * 单位时间内触发事件
@@ -14,7 +13,6 @@ export function limitAction(max: number, action: Function): void {
   max = max < 2 ? 2 : max
   let now = Number(Date.now())
   let scop = (max - 1) * 60 * 1000
-  let timeLock = isClient ? Number(localStorage.getItem('timeLockStr')) : timeLockMemory
 
   // 如果时间锁时间小于当前+timeEnd,则放行
   if (timeLock > now + scop) return
@@ -22,8 +20,6 @@ export function limitAction(max: number, action: Function): void {
   // 如果时间锁时间小于当前时间-timeEnd,则重置为当前时间并放行
   if (timeLock < now - scop) timeLock = now
   timeLock += 60000
-  if (isClient) localStorage.setItem('timeLockStr', String(timeLock))
-  else timeLockMemory = timeLock
 
   // 调用action方法
   action()
