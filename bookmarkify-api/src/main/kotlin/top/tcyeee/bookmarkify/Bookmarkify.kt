e227a3ae44cbaaf870a1d6d230cce66863fd8646
tcyeee <tcyeee@outlook.com>
2025-12-09 12:03:40 +0800

    Add ConfigurationPropertiesScan annotation to Bookmarkify and remove unnecessary Configuration annotation from ProjectConfig for improved configuration management.

--- bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/Bookmarkify.kt
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/Bookmarkify.kt
@@ -1,6 +1,7 @@
 package top.tcyeee.bookmarkify
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.transaction.annotation.EnableTransactionManagement
@@ -8,6 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement
 @EnableScheduling
 @SpringBootApplication
 @EnableTransactionManagement
+@ConfigurationPropertiesScan
 class Bookmarkify
 
 fun main(args: Array<String>) {
--- bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/config/entity/ProjectConfig.kt
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/config/entity/ProjectConfig.kt
@@ -1,13 +1,11 @@
 package top.tcyeee.bookmarkify.config.entity
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
 
 /**
  * @author tcyeee
  * @date 4/22/23 13:44
  */
-@Configuration
 @ConfigurationProperties(prefix = "config")
 data class ProjectConfig (
     var imgPath: String,          // 图片本地存储地址
