a525eb314233989709011e836ff3126bfa778fd4
tcyeee <tcyeee@outlook.com>
2025-12-11 10:49:39 +0800

    integrate Umami analytics script in nuxt.config

--- bookmarkify-web/layouts/setting.vue
+++ bookmarkify-web/layouts/setting.vue
@@ -1,32 +1,35 @@
 <template>
-    <CommonHeader class="fixed top-0 z-1" />
-    <div class="bg-gray-200 h-screen pt-[7rem]">
-    <div class="flex gap-10 sm:px-[1vw]  md:px-[2vw]  lg:px-[4vw] xl:px-[6vw] pb-[10vh] bg-gray-200">
+  <CommonHeader class="fixed top-0 z-1" />
+  <div class="bg-gray-200 h-screen pt-28">
+    <div class="flex gap-10 sm:px-[1vw] md:px-[2vw] lg:px-[4vw] xl:px-[6vw] pb-[10vh] bg-gray-200">
       <div>
         <NuxtLink to="/">
-          <div class="cy-btn cy-btn-wide cy-btn-lg rounded-xl mb-[2rem]">返回</div>
+          <div class="cy-btn cy-btn-wide cy-btn-lg rounded-xl mb-8">返回</div>
         </NuxtLink>
 
         <!-- 侧边栏 -->
-        <ul class="cy-menu bg-white rounded-xl w-[10rem]">
-          <li v-for="(item,index) in pages">
-            <a class="text-gray-400 text-lg font-medium" @click="selectOne(index)" :class="sysStore.settingTabIndex==index?'cy-menu-active':''">
-              <span>{{item}}</span>
+        <ul class="cy-menu bg-white rounded-xl w-40">
+          <li v-for="(item, index) in pages">
+            <a
+              class="text-gray-400 text-lg font-medium"
+              @click="selectOne(index)"
+              :class="sysStore.settingTabIndex == index ? 'cy-menu-active' : ''">
+              <span>{{ item }}</span>
             </a>
           </li>
         </ul>
       </div>
-      <NuxtPage class="flex-1 rounded-xl min-h-screen " />
+      <NuxtPage class="flex-1 rounded-xl min-h-screen" />
     </div>
     <CommonFooter class="fixed bottom-0" />
   </div>
 </template>
 
 <script lang="ts" setup>
-const sysStore = useSysStore();
-const pages = ["个人资料", "书签管理", "偏好设置"];
+const sysStore = useSysStore()
+const pages = ['个人资料', '书签管理', '偏好设置']
 
 function selectOne(index: number) {
-  sysStore.settingTabIndex = index;
+  sysStore.settingTabIndex = index
 }
 </script>
