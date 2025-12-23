452a739af461882eb6d06741b20bf16797f424ab
tcyeee <tcyeee@outlook.com>
2025-12-23 15:09:50 +0800

    feat(nuxt.config): enhance SEO metadata and add Open Graph and Twitter Card support; update footer styling for better visibility

--- bookmarkify-web/components/common/footer.vue
+++ bookmarkify-web/components/common/footer.vue
@@ -7,22 +7,20 @@
         <span>Bookmarkify</span>
         <span class="text-gray-400">· 探索互联网更多可能</span>
       </div>
-      <div class="flex flex-wrap items-center gap-x-4 gap-y-2 text-xs sm:text-sm">
-        <a
-          href="https://beian.miit.gov.cn"
-          target="_blank"
-          rel="noreferrer"
-          class="transition-colors text-gray-300 hover:text-white">
+      <div class="flex flex-wrap items-center gap-x-4 gap-y-2 text-xs sm:text-sm text-gray-500">
+        <a href="https://beian.miit.gov.cn" target="_blank" rel="noreferrer" class="transition-colors hover:text-white">
           备案号: 浙ICP备18053383号-3
         </a>
-
-        <a href="mailto:tcyeee@outlook.com" class="transition-colors text-gray-300 hover:text-white">
-          联系：tcyeee@outlook.com
-        </a>
+        <a href="mailto:tcyeee@outlook.com" class="transition-colors hover:text-white"> tcyeee@outlook.com </a>
       </div>
 
-      <a href="https://wj.qq.com/s2/25315374/f339" target="_blank" rel="noreferrer" class="text-gray-500 sm:inline-block">
-        问题反馈
+      <a
+        href="https://wj.qq.com/s2/25315374/f339"
+        target="_blank"
+        rel="noreferrer"
+        class="flex items-center gap-2 text-gray-500 hover:text-white">
+        <span class="icon--chat-processing icon-size-19 text-gray-500"></span>
+        <span class="text-gray-500 ml-2">问题反馈</span>
       </a>
     </div>
   </footer>
