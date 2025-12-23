ea3f0636176be3c1b57b5512f100e09447430021
tcyeee <tcyeee@outlook.com>
2025-12-24 00:43:59 +0800

    refactor(setting): streamline account cancellation component and integrate it into the profile layout for improved user accessibility

--- bookmarkify-web/components/setting/account/cancel.vue
+++ bookmarkify-web/components/setting/account/cancel.vue
@@ -1,30 +1,26 @@
 <template>
-  <div>
-    <!-- 注销账户 -->
-    <div class="setting-title mt-25">注销账户</div>
-    <div class="setting-subtitle">注销后账号所有数据将被销毁并不可找回，请谨慎操作。</div>
-    <button class="cy-btn cy-btn-error rounded-2xl mt-2" @click="openDelDialog">注销账户</button>
+  <!-- 注销账户 -->
+  <button class="cy-btn cy-btn-error rounded-2xl mt-2" @click="openDelDialog">注销账户</button>
 
-    <!-- 注销账户对话框 -->
-    <dialog id="userDelDialog" class="cy-modal">
-      <div class="cy-modal-box">
-        <div class="flex items-end gap-2">
-          <span class="icon--warning icon-size-22 text-red-500"></span>
-          <h3 class="text-lg font-bold text-red-500">警告: 账户注销后将无法找回!</h3>
-        </div>
-        <p class="mt-5 setting-subtitle">注销前需要验证密码:</p>
-        <input v-model="data.password" type="password" placeholder="请输入密码" class="cy-input" />
-
-        <div class="cy-modal-action">
-          <button @click="closeDelDialog" class="cy-btn">取消</button>
-          <button :disabled="data.password.length == 0" class="cy-btn cy-btn-error" @click="accountDel()">确认注销!</button>
-        </div>
+  <!-- 注销账户对话框 -->
+  <dialog id="userDelDialog" class="cy-modal">
+    <div class="cy-modal-box">
+      <div class="flex items-end gap-2">
+        <span class="icon--warning icon-size-22 text-red-500"></span>
+        <h3 class="text-lg font-bold text-red-500">警告: 账户注销后将无法找回!</h3>
       </div>
-      <form method="dialog" class="cy-modal-backdrop">
-        <button @click="closeDelDialog">close</button>
-      </form>
-    </dialog>
-  </div>
+      <p class="mt-5 setting-subtitle">注销前需要验证密码:</p>
+      <input v-model="data.password" type="password" placeholder="请输入密码" class="cy-input" />
+
+      <div class="cy-modal-action">
+        <button @click="closeDelDialog" class="cy-btn">取消</button>
+        <button :disabled="data.password.length == 0" class="cy-btn cy-btn-error" @click="accountDel()">确认注销!</button>
+      </div>
+    </div>
+    <form method="dialog" class="cy-modal-backdrop">
+      <button @click="closeDelDialog">close</button>
+    </form>
+  </dialog>
 </template>
 
 <script lang="ts" setup>
@@ -56,5 +52,3 @@ function closeDelDialog() {
   data.password = ''
 }
 </script>
-
-<style></style>
