47f32dfe375b292bb6f708715c8df1c1017a7397
tcyeee <tcyeee@outlook.com>
2025-12-23 17:17:01 +0800

    feat(setting): update SettingButton to include new memory-themed icons and enhance interaction with home click functionality

--- bookmarkify-web/components/home/TimeStr.vue
+++ bookmarkify-web/components/home/TimeStr.vue
@@ -1,30 +1,29 @@
 <template>
-  <div class="cursor-pointer">{{data.currentTime}}</div>
+  <div class="cursor-pointer select-none">{{ data.currentTime }}</div>
 </template>
 
 <script lang="ts" setup>
-const now = new Date();
+const now = new Date()
 const data = reactive<{
-  currentTime: string;
+  currentTime: string
 }>({
   currentTime: getCurrentTimeStr(now),
-});
+})
 
 onMounted(() => {
   // 对时间进行更新
-  setInterval(() => updateTime(), 1000);
-});
+  setInterval(() => updateTime(), 1000)
+})
 
 function getCurrentTimeStr(current: Date): string {
-  let minutes: string = current.getMinutes().toString();
-  minutes = current.getMinutes() < 10 ? `0${minutes}` : minutes;
-  return `${current.getHours().toString()} : ${minutes}`;
+  let minutes: string = current.getMinutes().toString()
+  minutes = current.getMinutes() < 10 ? `0${minutes}` : minutes
+  return `${current.getHours().toString()} : ${minutes}`
 }
 
 function updateTime() {
-  data.currentTime = getCurrentTimeStr(new Date());
+  data.currentTime = getCurrentTimeStr(new Date())
 }
 </script>
 
-<style>
-</style>
+<style></style>
