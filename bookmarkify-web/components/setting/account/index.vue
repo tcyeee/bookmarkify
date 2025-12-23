66d587c0a044cfff5ac17533b9d33384efb2af39
tcyeee <tcyeee@outlook.com>
2025-12-23 17:17:01 +0800

    feat(setting): update SettingButton to include new memory-themed icons and enhance interaction with home click functionality

--- bookmarkify-web/components/setting/account/index.vue
+++ bookmarkify-web/components/setting/account/index.vue
@@ -3,14 +3,14 @@
     <!-- 个人资料 -->
     <SettingAccountProfile v-if="userStore.account" :info="userStore.account" />
 
-    <!-- 账户绑定 -->
-    <SettingAccountVerify />
+    <!-- TODO 账户绑定 -->
+    <!-- <SettingAccountVerify /> -->
 
     <!-- 退出登录 -->
     <SettingAccountLogout v-if="userStore.account" />
 
-    <!-- 注销账户 -->
-    <SettingAccountCancel />
+    <!-- TODO 注销账户 -->
+    <!-- <SettingAccountCancel /> -->
   </div>
 </template>
 
