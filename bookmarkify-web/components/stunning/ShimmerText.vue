7170006c63aede3858ffefeba3b56198bb1c6957
tcyeee <tcyeee@outlook.com>
2025-12-23 12:41:18 +0800

    enhance user engagement on welcome page

---
+++ bookmarkify-web/components/stunning/ShimmerText.vue
@@ -0,0 +1,50 @@
+<template>
+  <p
+    :style="{
+      '--shimmer-width': `${shimmerWidth}px`,
+    }"
+    :class="
+      cn(
+        'mx-auto max-w-md text-neutral-600/50 dark:text-neutral-400/50 ',
+
+        // Shimmer effect
+        'animate-shimmer bg-clip-text bg-no-repeat bg-position-[0_0] bg-size-[var(--shimmer-width)_100%] [transition:background-position_1s_cubic-bezier(.6,.6,0,1)_infinite]',
+
+        // Shimmer gradient
+        'bg-linear-to-r from-transparent via-black/80 via-50% to-transparent dark:via-white/80'
+      )
+    ">
+    <slot />
+  </p>
+</template>
+
+<script lang="ts" setup>
+import { cn } from '@utils'
+
+withDefaults(
+  defineProps<{
+    shimmerWidth: number
+  }>(),
+  {
+    shimmerWidth: 100,
+  }
+)
+</script>
+
+<style scoped>
+@keyframes shimmer {
+  0%,
+  90%,
+  100% {
+    background-position: calc(-100% - var(--shimmer-width)) 0;
+  }
+  30%,
+  60% {
+    background-position: calc(100% + var(--shimmer-width)) 0;
+  }
+}
+
+.animate-shimmer {
+  animation: shimmer 8s infinite;
+}
+</style>
--- bookmarkify-web/pages/welcome.vue
+++ bookmarkify-web/pages/welcome.vue
@@ -21,6 +21,18 @@
           <p class="max-w-2xl text-base md:text-lg text-white/80">
             把常用的 Web 应用、工具和灵感集中收藏，随时同步、搜索与分享，让每一次打开都更高效。
           </p>
+
+          <!-- 开始使用按钮 -->
+          <div
+            class="group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800">
+            <ShimmerText
+              :shimmerWidth="100"
+              class="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+              ✨ 开始使用
+              <p
+                class="icon--arrow-right icon-size-3 w-4 h-4 ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+            </ShimmerText>
+          </div>
         </div>
 
         <!-- 查看更多按钮 -->
@@ -106,6 +118,8 @@ import { onBeforeUnmount, onMounted, ref } from 'vue'
 import FloatingBookmarks from '../components/welcome/FloatingBookmarks.vue'
 import SiteFooter from '../components/welcome/SiteFooter.vue'
 import UiScrollReveal from '../components/stunning/ScrollReveal.vue'
+import ShimmerText from '../components/stunning/ShimmerText.vue'
+
 const featureSection = ref<HTMLElement | null>(null)
 const handleScroll = () => {
   featureSection.value?.scrollIntoView({ behavior: 'smooth', block: 'start' })
