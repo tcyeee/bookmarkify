5e4395538aed8f35f1bdc9d2a74497d099cdd7c9
tcyeee <tcyeee@outlook.com>
2025-12-26 13:56:13 +0800

    feat(utils): add MailUtils class for sending emails via WeChat Work API with access token management

---
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/utils/MailUtils.kt
@@ -0,0 +1,65 @@
+package top.tcyeee.bookmarkify.utils
+
+import cn.hutool.http.HttpUtil
+import cn.hutool.json.JSONUtil
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import top.tcyeee.bookmarkify.config.cache.RedisType
+import top.tcyeee.bookmarkify.config.exception.CommonException
+import top.tcyeee.bookmarkify.config.exception.ErrorType
+import top.tcyeee.bookmarkify.config.log
+
+/**
+ * 邮件工具类
+ * 
+ * @author tcyeee
+ */
+@Component
+class MailUtils {
+    
+    @Value("\${wechat.work.corpid}")
+    private lateinit var corpid: String
+
+    @Value("\${wechat.work.corpsecret}")
+    private lateinit var corpsecret: String
+
+    private fun getAccessToken(): String {
+        RedisUtils.get<String>(RedisType.WECHAT_WORK_ACCESS_TOKEN, "system")?.let { return it }
+
+        val url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$corpid&corpsecret=$corpsecret"
+        val response = HttpUtil.get(url)
+        val json = JSONUtil.parseObj(response)
+        if (json.getInt("errcode") != 0) {
+            throw CommonException(ErrorType.E216, "获取企业微信AccessToken失败: ${json.getStr("errmsg")}")
+        }
+        val token = json.getStr("access_token")
+        RedisUtils.set(RedisType.WECHAT_WORK_ACCESS_TOKEN, "system", token)
+        return token
+    }
+
+    fun send(to: String, subject: String, content: String): Boolean {
+        try {
+            val accessToken = getAccessToken()
+            val url = "https://qyapi.weixin.qq.com/cgi-bin/exmail/app/compose_send?access_token=$accessToken"
+
+            val payload = mapOf(
+                "to" to mapOf("emails" to listOf(to)),
+                "subject" to subject,
+                "content" to content
+            )
+
+            val response = HttpUtil.post(url, JSONUtil.toJsonStr(payload))
+            val json = JSONUtil.parseObj(response)
+
+            if (json.getInt("errcode") == 0) {
+                return true
+            } else {
+                log.error("发送邮件失败: ${json.getStr("errmsg")}")
+                return false
+            }
+        } catch (e: Exception) {
+            log.error("发送邮件异常", e)
+            return false
+        }
+    }
+}
