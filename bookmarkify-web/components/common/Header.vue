82d9e8b56cba38f1e34d65d76f981d8766304e94
tcyeee <tcyeee@outlook.com>
2025-12-26 14:23:29 +0800

    feat(header): add click navigation to header for improved user experience

--- bookmarkify-web/components/common/Header.vue
+++ bookmarkify-web/components/common/Header.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="px-4 h-16 bg-white w-full overflow-hidden shadow flex items-center">
+  <div
+    class="px-4 h-16 bg-white w-full overflow-hidden shadow flex items-center select-none cursor-pointer"
+    @click="navigateTo('/')">
     <div class="flex items-center gap-2">
       <div class="icon--memory-device icon-size-30 hover:text-white"></div>
       <div class="font-jersey10 font-bold text-4xl">bookmarkify</div>
