eaca854df16f53c690d13dbda245dcc42dffad60
tcyeee <tcyeee@outlook.com>
2025-12-28 10:37:45 +0800

    refactor(bookmark): improve code formatting and clean up imports

--- bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/controller/bookmark/BookmarkController.kt
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/controller/bookmark/BookmarkController.kt
@@ -1,26 +1,17 @@
 package top.tcyeee.bookmarkify.controller.bookmark
 
 import cn.dev33.satoken.annotation.SaIgnore
-import cn.hutool.core.util.IdUtil
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.multipart.MultipartFile
-import top.tcyeee.bookmarkify.config.log
-import top.tcyeee.bookmarkify.entity.BookmarkDetail
+
 import top.tcyeee.bookmarkify.entity.BookmarkUpdataPrams
 import top.tcyeee.bookmarkify.entity.HomeItemShow
 import top.tcyeee.bookmarkify.entity.HomeItemSortParams
-import top.tcyeee.bookmarkify.entity.entity.Bookmark
-import top.tcyeee.bookmarkify.entity.entity.BookmarkUserLink
-import top.tcyeee.bookmarkify.entity.entity.HomeItem
 import top.tcyeee.bookmarkify.server.IBookmarkService
 import top.tcyeee.bookmarkify.server.IBookmarkUserLinkService
 import top.tcyeee.bookmarkify.server.IHomeItemService
 import top.tcyeee.bookmarkify.utils.BaseUtils
-import java.util.function.Consumer
-import java.util.stream.Collectors
 
 /**
  * @author tcyeee
@@ -44,8 +35,7 @@ class BookmarksController(
 
     @PostMapping("/sort")
     @Operation(summary = "排序")
-    fun sort(@RequestBody params: List<HomeItemSortParams>): Boolean = true
-        .also { homeItemService.sort(params) }
+    fun sort(@RequestBody params: List<HomeItemSortParams>): Boolean = true.also { homeItemService.sort(params) }
 
     @PostMapping("/delete")
     @Operation(summary = "删除(仅删除桌面排序)")
