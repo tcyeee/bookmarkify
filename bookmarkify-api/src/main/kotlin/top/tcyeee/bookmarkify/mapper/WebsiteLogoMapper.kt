2fb5436d5f9832613e2bfc384526a5be0e9053d7
tcyeee <tcyeee@outlook.com>
2025-12-29 16:46:10 +0800

    feat(WebsiteLogo): add WebsiteLogoMapper, IWebsiteLogoService, and WebsiteLogoServiceImpl; implement service methods for logo retrieval by bookmark ID

---
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/mapper/WebsiteLogoMapper.kt
@@ -0,0 +1,14 @@
+package top.tcyeee.bookmarkify.mapper
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper
+import org.apache.ibatis.annotations.Mapper
+import top.tcyeee.bookmarkify.entity.entity.WebsiteLogoEntity
+
+/**
+ * 网站Logo Mapper
+ *
+ * @author tcyeee
+ * @date 12/29/25 15:05
+ */
+@Mapper
+interface WebsiteLogoMapper : BaseMapper<WebsiteLogoEntity>
---
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/server/IWebsiteLogoService.kt
@@ -0,0 +1,12 @@
+package top.tcyeee.bookmarkify.server
+
+import com.baomidou.mybatisplus.extension.service.IService
+import top.tcyeee.bookmarkify.entity.entity.WebsiteLogoEntity
+
+/**
+ * 网站Logo Service
+ *
+ * @author tcyeee
+ * @date 12/29/25 15:05
+ */
+interface IWebsiteLogoService : IService<WebsiteLogoEntity>
