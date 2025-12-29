db714b48b65caea66dd613f4446ac49890fe87a7
tcyeee <tcyeee@outlook.com>
2025-12-29 15:33:07 +0800

    feat(WebsiteLogo): add WebsiteLogoEntity and file type definitions; implement OSS utility for file management

---
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/entity/enums/FileType.kt
@@ -0,0 +1,7 @@
+package top.tcyeee.bookmarkify.entity.enums
+
+/**
+ * @author tcyeee
+ * @date 12/29/25 13:56
+ */enum class FileType {
+}
---
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/utils/OssUtils.kt
@@ -0,0 +1,6 @@
+package top.tcyeee.bookmarkify.utils
+
+/**
+ * @author tcyeee
+ * @date 12/29/25 13:43
+ */
