dfa637dff30001f620ec57b3128910b0f8806397
tcyeee <tcyeee@outlook.com>
2026-01-01 22:10:50 +0800

    refactor(user-preference): enhance UserPreferenceEntity with additional fields for user settings and improve related components

---
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/controller/UserPreferenceController.kt
@@ -0,0 +1,26 @@
+package top.tcyeee.bookmarkify.controller
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.*
+import top.tcyeee.bookmarkify.entity.entity.UserPreferenceEntity
+import top.tcyeee.bookmarkify.server.IUserPreferenceService
+import top.tcyeee.bookmarkify.utils.BaseUtils
+
+@RestController
+@Tag(name = "用户偏好设置")
+@RequestMapping("/preference")
+class UserPreferenceController(
+    private val userPreferenceService: IUserPreferenceService
+) {
+
+    @GetMapping
+    @Operation(summary = "获取当前用户偏好设置")
+    fun query(): UserPreferenceEntity? = userPreferenceService.queryByUid(BaseUtils.uid())
+
+    @PostMapping
+    @Operation(summary = "更新当前用户偏好设置")
+    fun update(@RequestBody params: UserPreferenceEntity): Boolean =
+        userPreferenceService.upsertByUid(BaseUtils.uid(), params)
+}
+
---
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/server/IUserPreferenceService.kt
@@ -0,0 +1,10 @@
+package top.tcyeee.bookmarkify.server
+
+import com.baomidou.mybatisplus.extension.service.IService
+import top.tcyeee.bookmarkify.entity.entity.UserPreferenceEntity
+
+interface IUserPreferenceService : IService<UserPreferenceEntity> {
+    fun queryByUid(uid: String): UserPreferenceEntity?
+    fun upsertByUid(uid: String, params: UserPreferenceEntity): Boolean
+}
+
