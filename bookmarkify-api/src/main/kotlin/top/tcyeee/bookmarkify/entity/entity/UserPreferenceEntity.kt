5c2eef580b0d3a0938421ae89618c120bcd9426f
tcyeee <tcyeee@outlook.com>
2026-01-01 22:05:56 +0800

    chore(dependencies): update package manager version to pnpm@10.27.0 and add new fields to UserPreferenceEntity for title visibility and page turn mode

--- bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/entity/entity/UserPreferenceEntity.kt
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/entity/entity/UserPreferenceEntity.kt
@@ -15,6 +15,8 @@ data class UserPreferenceEntity(
     @field:Schema(description = "书签打开方式") var bookmarkOpenMode: BookmarkOpenMode = BookmarkOpenMode.CURRENT_TAB,
     @field:Schema(description = "是否开启极简模式") var minimalMode: Boolean = false,
     @field:Schema(description = "书签排列方式") var bookmarkLayout: BookmarkLayoutMode = BookmarkLayoutMode.DEFAULT,
+    @field:Schema(description = "是否显示标题") var showTitle: Boolean = true,
+    @field:Schema(description = "翻页方式") var pageMode: PageTurnMode = PageTurnMode.VERTICAL_SCROLL,
 
     @field:Schema(description = "更新时间") var updateTime: LocalDateTime = LocalDateTime.now(),
     @field:Schema(description = "创建时间") var createTime: LocalDateTime = LocalDateTime.now(),
@@ -28,6 +30,9 @@ data class UserPreferenceEntity(
  * 2. 书签打开方式(当前/新页面)
  * 3. 极简模式开关
  * 4. 书签排列方式(紧凑/默认/空旷)
+ * 5. 是否显示标题(默认显示)
+ * 6. 翻页方式(垂直滚动/横向翻页)
  */
 enum class BookmarkOpenMode { CURRENT_TAB, NEW_TAB }
-enum class BookmarkLayoutMode { COMPACT, DEFAULT, SPACIOUS, }
+enum class BookmarkLayoutMode { COMPACT, DEFAULT, SPACIOUS }
+enum class PageTurnMode { VERTICAL_SCROLL, HORIZONTAL_PAGE }
