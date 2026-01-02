45737efd04590b2c91c1e3f3bcd78545326bbdfc
tcyeee <tcyeee@outlook.com>
2026-01-02 09:34:09 +0800

    refactor(bookmark): simplify Bookmark component by replacing inline image handling with BookmarkLogo component and removing unnecessary state management

--- bookmarkify-web/components/launchpad/cell/Bookmark.vue
+++ bookmarkify-web/components/launchpad/cell/Bookmark.vue
@@ -1,19 +1,6 @@
 <template>
   <div class="w-20 flex flex-col items-center">
-    <div
-      class="w-app h-app rounded-xl bg-gray-100 center shadow overflow-hidden"
-      :class="data.isDev ? 'border-4 border-dashed border-red-200' : ''">
-      <div class="h-20 w-20 rounded-2xl bg-white flex justify-center items-center">
-        <img v-if="props.value.iconHdUrl && !hdError" :src="props.value.iconHdUrl" alt="" @error="onHdError" />
-        <img
-          v-else-if="!iconError"
-          class="w-8 h-8"
-          :src="`data:image/png;base64,${props.value.iconBase64}`"
-          alt=""
-          @error="onIconError" />
-        <img v-else class="w-8 h-8" src="/avatar/default.png" alt="" />
-      </div>
-    </div>
+    <BookmarkLogo :value="props.value" />
     <div class="w-18 text-sm mt-[0.3rem] text-white opacity-90 truncate text-center">
       {{ props.value.title || props.value.urlBase }}
     </div>
@@ -21,37 +8,7 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, reactive, ref } from 'vue'
 import type { Bookmark } from '@typing'
+import BookmarkLogo from './BookmarkLogo.vue'
 const props = defineProps<{ value: Bookmark }>()
-const data = reactive<{ isDev: boolean }>({ isDev: false })
-const hdError = ref(false)
-const iconError = ref(false)
-
-onMounted(() => {
-  data.isDev = isLocalhostOrIP(props.value.urlFull)
-})
-
-function onHdError() {
-  hdError.value = true
-}
-
-function onIconError() {
-  iconError.value = true
-}
-
-function isLocalhostOrIP(url: string): boolean {
-  // 定义匹配 localhost 或者 IP 地址的正则表达式
-  const localhostRegex = /^(localhost|127\.0\.0\.1|::1)$/i
-  const ipRegex = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/
-
-  try {
-    // 获取 URL 的主机部分
-    const hostname = new URL(url).hostname
-    // 判断是否为 localhost 或者是 IP 地址
-    return localhostRegex.test(hostname) || ipRegex.test(hostname)
-  } catch {
-    return false
-  }
-}
 </script>
