e8a814020ae7800b124437b3863e3885a5b590e9
tcyeee <tcyeee@outlook.com>
2026-01-05 16:36:55 +0800

    refactor(auth-plugin): streamline user authentication flow by consolidating WebSocket connection and user info updates

--- bookmarkify-web/plugins/auth.ts
+++ bookmarkify-web/plugins/auth.ts
@@ -4,19 +4,17 @@ export default defineNuxtPlugin(async () => {
   const userStore = useUserStore()
   console.log(`DEBUG: 帐户状态为: ${userStore.authStatus}`)
 
-  // 如果用户已登录，则连接WebSocket
   if (userStore.authStatus !== AuthStatusEnum.NONE) {
+    // 如果用户已登录，则连接WebSocket
     const webSocketStore = useWebSocketStore()
     webSocketStore.connect(userStore.account!.token)
-  }
 
-  // 如果用户已经登录,则更新用户权限信息
-  if (userStore.authStatus !== AuthStatusEnum.NONE) {
+    // 如果用户已经登录,则更新用户权限信息
     userStore.refreshUserInfo()
-  }
+    // 如果用户已经登录,则更新用户偏好设置
+    userStore.fetchPreference()
 
-  // 如果用户已经登录,则更新书签信息
-  if (userStore.authStatus !== AuthStatusEnum.NONE) {
+    // 如果用户已经登录,则更新书签信息
     const bookmarkStore = useBookmarkStore()
     bookmarkStore.update()
   }
