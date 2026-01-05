ad8d63fbabbc0b0ca9177f4e2a851b03e4244315
tcyeee <tcyeee@outlook.com>
2026-01-05 19:22:54 +0800

    feat(settings): add BackgroundController and UserPreferenceController for managing background settings and user preferences

--- bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/controller/setting/UserPreferenceController.kt
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/controller/setting/UserPreferenceController.kt
@@ -1,11 +1,14 @@
-package top.tcyeee.bookmarkify.controller
+package top.tcyeee.bookmarkify.controller.setting
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import top.tcyeee.bookmarkify.entity.UserPreferenceUpdateParams
 import top.tcyeee.bookmarkify.entity.UserPreferenceVO
-import top.tcyeee.bookmarkify.entity.entity.UserPreferenceEntity
 import top.tcyeee.bookmarkify.server.IUserPreferenceService
 import top.tcyeee.bookmarkify.utils.BaseUtils
 
@@ -26,4 +29,3 @@ class UserPreferenceController(
         return userPreferenceService.upsertByUid(BaseUtils.uid(), params)
     }
 }
-
---
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/controller/user/AdminController.kt
@@ -0,0 +1,8 @@
+package top.tcyeee.bookmarkify.controller.user
+
+/**
+ * @author tcyeee
+ * @date 1/5/26 19:19
+ */
+class AdminController {
+}
