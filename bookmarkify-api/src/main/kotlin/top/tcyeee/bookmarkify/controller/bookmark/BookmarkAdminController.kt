1507a0a85d8d6573166a5adb8114d526b570881b
tcyeee <tcyeee@outlook.com>
2026-01-07 22:48:31 +0800

    feat(bookmark): add BookmarkAdminController and ParseStatusEnum for admin bookmark management

---
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/controller/bookmark/BookmarkAdminController.kt
@@ -0,0 +1,6 @@
+package top.tcyeee.bookmarkify.controller.bookmark
+
+/**
+ * @author tcyeee
+ * @date 1/7/26 22:13
+ */
---
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/entity/enums/ParseStatusEnum.kt
@@ -0,0 +1,7 @@
+package top.tcyeee.bookmarkify.entity.enums
+
+/**
+ * @author tcyeee
+ * @date 1/7/26 22:41
+ */enum class ParseStatusEnum {
+}
