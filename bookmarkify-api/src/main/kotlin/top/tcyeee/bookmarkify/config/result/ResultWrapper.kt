7ff202b3e545e05cde0a109942fe309775c1383f
tcyeee <tcyeee@outlook.com>
2026-01-08 18:57:01 +0800

    refactor(result): remove unused pagination fields from ResultWrapper

--- bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/config/result/ResultWrapper.kt
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/config/result/ResultWrapper.kt
@@ -8,13 +8,8 @@ data class ResultWrapper(
     @field:Schema(name = "是否成功, true 是 false 否") var ok: Boolean = false,
     @field:Schema(name = "提示编码，也即 错误码，非 0 都是失败") var code: Int = 0,
     @field:Schema(name = "数据主体", nullable = true) var data: Any? = null,
-
     @field:JsonInclude(JsonInclude.Include.NON_NULL)
     @field:Schema(name = "错误提示，用户可阅读", nullable = true) var msg: String? = null,
-    @field:JsonInclude(JsonInclude.Include.NON_NULL)
-    @field:Schema(name = "当前页码", nullable = true) var currentPage: Int? = null,
-    @field:JsonInclude(JsonInclude.Include.NON_NULL)
-    @field:Schema(name = "全部页码", nullable = true) var total: Int? = null
 ) {
     companion object {
         fun ok(data: Any) = ResultWrapper(
