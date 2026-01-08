a2bef4cd9f10ff746fec37d4fc698fb74d7ad153
tcyeee <tcyeee@outlook.com>
2026-01-08 18:34:11 +0800

    feat(filter): add StpInterfaceImpl class skeleton

--- bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/config/filter/StpInterfaceImpl.kt
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/config/filter/StpInterfaceImpl.kt
@@ -1,7 +1,19 @@
 package top.tcyeee.bookmarkify.config.filter
 
+import cn.dev33.satoken.stp.StpInterface
+import org.springframework.stereotype.Component
+import top.tcyeee.bookmarkify.mapper.UserMapper
+
 /**
  * @author tcyeee
  * @date 1/8/26 18:27
- */class StpInterfaceImpl {
+ */
+@Component
+class StpInterfaceImpl(
+    private val userMapper: UserMapper
+) : StpInterface {
+    override fun getRoleList(loginId: Any, loginType: String?): List<String> =
+        userMapper.selectById(loginId.toString())?.let { listOf(it.role.name) } ?: emptyList()
+
+    override fun getPermissionList(loginId: Any, loginType: String?): List<String> = emptyList()
 }
