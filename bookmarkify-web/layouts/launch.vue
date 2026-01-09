0d82495b39e20f21fc1eaac9b2b58026d1962ac8
tcyeee <tcyeee@outlook.com>
2026-01-09 16:23:29 +0800

    refactor(components): simplify async component imports and fix grid layout

--- bookmarkify-web/layouts/launch.vue
+++ bookmarkify-web/layouts/launch.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="overflow-hidden select-none w-screen h-screen px-4 sm:px-[7vw] md:px-[10vw] lg:px-[12vw] pt-[10vh]">
-    <div class="w-full h-full bg-gray-100">
+    <div class="w-full h-full">
         <NuxtPage />
     </div>
   </div>
