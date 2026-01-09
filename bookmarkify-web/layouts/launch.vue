09fddcc461ba549f12f3234ed0d6772d580e9040
tcyeee <tcyeee@outlook.com>
2026-01-09 10:49:29 +0800

    feat(bookmark-cleaning): add detail dialog for bookmark entity display
    Implement a dialog to show detailed information about a selected bookmark entity when a grid cell is clicked. This enhances user interaction by providing a clearer view of bookmark attributes such as app name, URL, and status.

---
+++ bookmarkify-web/layouts/launch.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="overflow-hidden select-none w-screen h-screen px-4 sm:px-[7vw] md:px-[10vw] lg:px-[12vw] pt-[10vh]">
+    <div class="w-full h-full bg-gray-100">
+        <NuxtPage />
+    </div>
+  </div>
+</template>
