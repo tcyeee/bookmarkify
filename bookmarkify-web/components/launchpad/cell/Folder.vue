25fd3b7329b356a4cc49cb57aad8e050708e4882
tcyeee <tcyeee@outlook.com>
2026-01-13 14:07:13 +0800

    refactor(launchpad): streamline component rendering logic in Item.vue and Folder.vue

--- bookmarkify-web/components/launchpad/cell/Folder.vue
+++ bookmarkify-web/components/launchpad/cell/Folder.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-20 flex flex-col items-center" :class="{ 'justify-center': !showTitle }" @click="onClick">
-    <div class="w-app h-app rounded-xl bg-gray-300 flex justify-center items-center shadow">
-      <div>
+    <div class="rounded-xl bg-gray-300 flex justify-center items-center shadow" :style="folderSizeStyle">
+      dir
         <!-- TODO 文件夹的显示,需要4个LOGO进行填充 -->
         <!-- <div class="flex mb-[0.4rem]">
           <BookmarkLogo v-if="bookmarkList.length > 0" sm :bookmark="bookmarkList[0]!" class="mr-[0.4rem]" />
@@ -11,9 +11,8 @@
           <BookmarkLogo v-if="bookmarkList.length > 2" sm :bookmark="bookmarkList[2]!" class="mr-[0.4rem]" />
           <BookmarkLogo v-if="bookmarkList.length > 3" sm :bookmark="bookmarkList[3]!" />
         </div> -->
-      </div>
     </div>
-    <div v-if="showTitle" class="w-18 text-xs mt-[0.3rem] text-gray-800 truncate text-center">{{ value.name }}</div>
+    <div v-if="showTitle" class="w-18 text-xs mt-[0.3rem] text-gray-800 truncate text-center">{{ value.name||'dir' }}</div>
   </div>
 </template>
 
@@ -29,6 +28,11 @@ const { value, toggleDrag } = toRefs(props)
 
 const preferenceStore = usePreferenceStore()
 const showTitle = computed<boolean>(() => preferenceStore.preference?.showTitle ?? true)
+const folderAreaSize = computed(() => preferenceStore.bookmarkCellSizePx)
+const folderSizeStyle = computed(() => ({
+  width: `${folderAreaSize.value}px`,
+  height: `${folderAreaSize.value}px`,
+}))
 // const bookmarkList = computed(() => value.value?.bookmarkList ?? [])
 
 function onClick() {
