8153dda11db025fdae1bbc06d15111bd86c44f67
tcyeee <tcyeee@outlook.com>
2026-03-18 16:35:01 +0800

    refactor(launchpad): move settings modal from page to layout

--- bookmarkify-web/layouts/launch.vue
+++ bookmarkify-web/layouts/launch.vue
@@ -5,7 +5,7 @@
     <div class="w-full">
       <NuxtPage />
     </div>
-    <CommonAddBookmarkFab v-if="showDesktopAddEntry" />
+    <LaunchpadSettingsModal />
   </div>
 </template>
 
@@ -17,7 +17,6 @@ import { usePreferenceStore } from '@stores/preference.store'
 
 const preferenceStore = usePreferenceStore()
 const backgroundSetting = computed<BacSettingVO | null>(() => preferenceStore.preference?.imgBacShow ?? null)
-const showDesktopAddEntry = computed<boolean>(() => preferenceStore.preference?.showDesktopAddEntry ?? true)
 
 const backgroundUrl = computed(() => {
   const cfg = backgroundSetting.value
