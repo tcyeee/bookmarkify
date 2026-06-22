8db8c78541136b2ecf7728a6254c0af93939066c
tcyeee <tcyeee@outlook.com>
2026-06-23 00:34:04 +0800

    feat(api): add website category dictionary CRUD endpoints

--- bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/server/IWebsiteCategoryService.kt
+++ bookmarkify-api/src/main/kotlin/top/tcyeee/bookmarkify/server/IWebsiteCategoryService.kt
@@ -1,9 +1,19 @@
 package top.tcyeee.bookmarkify.server
 
 import com.baomidou.mybatisplus.extension.service.IService
+import top.tcyeee.bookmarkify.entity.CategorySaveParams
 import top.tcyeee.bookmarkify.entity.entity.WebsiteCategory
 
 interface IWebsiteCategoryService : IService<WebsiteCategory> {
     /** 全部启用的分类，按 sort 升序 */
     fun activeCandidates(): List<WebsiteCategory>
+
+    /** 后台：全部未删除分类，按 sort 升序 */
+    fun listAll(): List<WebsiteCategory>
+
+    /** 后台：新增或修改一条分类（id 为空=新增） */
+    fun saveCategory(params: CategorySaveParams): WebsiteCategory
+
+    /** 后台：软删一条分类 */
+    fun softDelete(id: String)
 }
